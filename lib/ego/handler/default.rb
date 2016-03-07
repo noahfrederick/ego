45f1d0c5d151c1c77b51735e40f18f318b58fdf3
Noah Frederick <noah@noahfrederick.com>
2016-03-06 22:36:26 -0500

    Print handler template in default handler

--- lib/ego/handler/default.rb
+++ lib/ego/handler/default.rb
@@ -1,3 +1,11 @@
 handle /(.*)/, priority: 0 do |robot, matches|
   robot.respond %Q{I don't understand "#{matches[0]}".}
+
+  STDERR.puts <<-EOF
+Perhaps add a handler to #{Ego::Filesystem.config 'handler/{}.rb'}:
+
+handle /^#{matches[0]}$/i do |robot|
+  # Do it
+end
+EOF
 end
