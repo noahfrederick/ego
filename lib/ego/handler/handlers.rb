7751ea538b73d1947b29860bcc9a439b48e71c9c
Noah Frederick <noah@noahfrederick.com>
2016-03-12 16:33:58 -0500

    Broaden patterns for "handlers" handler

--- lib/ego/handler/handlers.rb
+++ lib/ego/handler/handlers.rb
@@ -3,8 +3,8 @@ require_relative '../handler'
 Ego::Handler.register do |handler|
   handler.description = 'tell you what I can do'
 
-  handler.listen /^(show me|show|list)\s+(handlers|what you can do|what you are able to do|what you do)$/i
-  handler.listen /^what (?:can you|are you able to|do you) (?:do|handle)\??$/i
+  handler.listen /^(show me|show|tell me|list)\s+(handlers|what you can do|what (?:you are|you're) able to do|what you do|what (?:queries )?you (?:can )?understand)$/i
+  handler.listen /^what (?:can you|are you able to|do you) (?:do|handle|understand)\??$/i
 
   handler.run do |robot|
     robot.respond 'I know how to...'
