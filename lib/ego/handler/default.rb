f84f367c67a2706ad5740d2f1bf964d188b2d52c
Noah Frederick <noah@noahfrederick.com>
2017-03-09 15:17:36 -0500

    Make handler slug suggestion more robust

--- lib/ego/handler/default.rb
+++ lib/ego/handler/default.rb
@@ -9,8 +9,8 @@ Ego::Handler.register do |handler|
     handler_slug = params[0]
       .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
       .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-      .tr('-', '_')
-      .gsub(/\s/, '_')
+      .tr('\'', '')
+      .gsub(/\W+/, '_')
       .gsub(/__+/, '_')
       .downcase
 
