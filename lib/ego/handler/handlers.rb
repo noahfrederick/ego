7dadf4549fdc98bfc56151a5502485f6eb9895d7
Noah Frederick <code@noahfrederick.com>
2017-08-29 14:52:05 -0400

    Listen for 'help...'

--- lib/ego/handler/handlers.rb
+++ lib/ego/handler/handlers.rb
@@ -5,6 +5,7 @@ Ego::Handler.register do |handler|
 
   handler.listen /^(show me|show|tell me|list)\s+(handlers|what you can do|what (?:you are|you're) able to do|what you do|what (?:queries )?you (?:can )?understand)$/i
   handler.listen /^what (?:can you|are you able to|do you) (?:do|handle|understand)\??$/i
+  handler.listen /^help/i, priority: 2
 
   handler.run do |robot|
     robot.respond 'I know how to...'
